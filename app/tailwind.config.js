40f7a402dd24d227a921b77ee0c952eb730cd357
Klaus Betz <klaus.betz@deckweiss.at>
2022-11-07 21:45:37 +0100

    cleanup eslint

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,15 +6,6 @@ module.exports = {
     extend: {
       colors: {
         primary: '#ec6726'
-      },
-      animation: {
-        'pulse-hard-pause': 'pulse-hard-pause 2s ease-in-out infinite'
-      },
-      keyframes: {
-        'pulse-hard-pause': {
-          '0%, 66%': { opacity: '1' },
-          '33%': { opacity: '0.3' }
-        }
       }
     }
   },
